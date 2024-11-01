18493c6ea0fc03191ee32eb0313db8b715b0f370
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-11-01 04:02:39 +0400

    Add files via upload

---
+++ 31prop.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public delegate bool MatchDelegate<T>(T item);
+
+    public static void Main()
+    {
+        List<Person> people = new List<Person>
+        {
+            new Person { Name = "Anna", Age = 33 },
+            new Person { Name = "Daviti", Age = 30 },
+            new Person { Name = "Dina", Age = 31 },
+            new Person { Name = "Daviti", Age = 30 },
+            new Person { Name = "Daviti", Age = 35 },
+            new Person { Name = "Daviti", Age = 30 }
+        };
+
+        Person foundPerson = FindFirstMatch(people, p => IsMatch(p, "Daviti", 30));
+        if (foundPerson != null)
+        {
+            Console.WriteLine($"Found: {foundPerson.Name},{foundPerson.Age}");
+        }
+        else
+        {
+            Console.WriteLine("Object does not exist");
+        }
+
+        Console.WriteLine("\nIndexes:");
+        FindAndDisplayAllMatches(people, p => IsMatch(p, "Daviti", 30));
+    }
+
+    public static T FindFirstMatch<T>(List<T> list, MatchDelegate<T> matchDelegate)
+    {
+        foreach (var item in list)
+        {
+            if (matchDelegate(item))
+            {
+                return item;
+            }
+        }
+        return default;
+    }
+
+    public static void FindAndDisplayAllMatches<T>(List<T> list, MatchDelegate<T> matchDelegate)
+    {
+        List<int> matches = new List<int>();
+        for (int index = 0; index < list.Count; index++)
+        {
+            if (matchDelegate(list[index]))
+            {
+                Console.WriteLine($"{index}");
+                matches.Add(index);
+            }
+        }
+    }
+
+    public static bool IsMatch(Person person, string name, int age)
+    {
+        return person.Name == name && person.Age == age;
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
---
+++ Program31intstr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public delegate bool MatchDelegate<T>(T item, T searchValue);
+
+    public static void Main()
+    {
+        List<string> stringList = new List<string> { "pink", "cat", "cherry", "cat", "tail" };
+        string searchString = "cat";
+
+        Console.WriteLine($"results for\"{searchString}\":");
+        FindAndDisplayMatches(stringList, searchString, IsMatch);
+
+        List<int> intList = new List<int> { -8, 2, 3, 2, 4, 5, 2 };
+        int searchInt = 2;
+
+        Console.WriteLine($"\nresults for {searchInt}:");
+        FindAndDisplayMatches(intList, searchInt, IsMatch);
+    }
+    public static void FindAndDisplayMatches<T>(List<T> list, T searchValue, MatchDelegate<T> matchDelegate)
+    {
+        for (int index = 0; index < list.Count; index++)
+        {
+            if (matchDelegate(list[index], searchValue))
+            {
+                Console.WriteLine($"{index}");
+            }
+        }
+    }
+
+    public static bool IsMatch<T>(T item, T searchValue)
+    {
+        return EqualityComparer<T>.Default.Equals(item, searchValue);
+    }
+}
